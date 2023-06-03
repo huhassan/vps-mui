22b45bad074e336290ecacf88749b0ce34d31c91
Hussam Hassan <hussam@tuta.io>
2023-06-03 04:50:15 +0300

    adding mui button

--- pages/index/index.page.tsx
+++ pages/index/index.page.tsx
@@ -1,15 +1,16 @@
-import { Counter } from './Counter'
-import Menu from '@mui/icons-material/Menu'
+import { Button } from "@mui/material";
+import { Counter } from "./Counter";
+import Menu from "@mui/icons-material/Menu";
 // Workaround for https://github.com/mui/material-ui/issues/37375 (see https://github.com/brillout/vite-plugin-ssr/discussions/901 for more info).
 // Instead of having to do this every time we import a MUI component, we can:
 //  - Add MUI to ssr.noExternal for the production build: https://github.com/brillout/vite-plugin-ssr/discussions/901#discussioncomment-5985771
 //  - Use https://github.com/cyco130/vite-plugin-cjs-interop for automatically applying this workaround
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-const MenuIcon: typeof Menu = Menu.default ?? Menu
-console.log('MenuIcon', MenuIcon)
+const MenuIcon: typeof Menu = Menu.default ?? Menu;
+console.log("MenuIcon", MenuIcon);
 
-export { Page }
+export { Page };
 
 function Page() {
   return (
@@ -23,6 +24,7 @@ function Page() {
           Interactive. <Counter />
         </li>
       </ul>
+      <Button variant="outlined">Click Me!</Button>
     </>
-  )
+  );
 }
